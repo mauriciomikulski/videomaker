73d8ddadbff7eae8469a4c7fbcb65a3d41655520
mauriciomikulski <incognitomikulski@gmail.com>
2019-03-20 08:19:00 -0300

    feat: add watson language understanding service

--- index.js
+++ index.js
@@ -1,22 +1,31 @@
 const readline = require('readline-sync')
-
-function start() {
-    const content = {}
-
-    content.searchTerm = askAndReturnSearchTerm()
-    content.prefix = askAndReturnPrefix()
-
-    function askAndReturnSearchTerm() {
-        return readline.question('Type a Wikipedia search term: ')
-    }
-    function askAndReturnPrefix() {
-        const prefixes = ['Who is', 'What is', 'The history of']
-        const selectedPrefixIndex = readline.keyInSelect(prefixes, 'Choose one option: ')
-        const selectedPrefixText = prefixes[selectedPrefixIndex]
-        return selectedPrefixText
-    }
-    console.log(content)
+const robots = {
+  text: require('./robots/text.js')
 }
 
+async function start() {
+  const content = {
+    maximumSentences: 7
+  }
+
+  content.searchTerm = askAndReturnSearchTerm()
+  content.prefix = askAndReturnPrefix()
+
+  await robots.text(content)
+
+  function askAndReturnSearchTerm() {
+    return readline.question('Type a Wikipedia search term: ')
+  }
+
+  function askAndReturnPrefix() {
+    const prefixes = ['Who is', 'What is', 'The history of']
+    const selectedPrefixIndex = readline.keyInSelect(prefixes, 'Choose one option: ')
+    const selectedPrefixText = prefixes[selectedPrefixIndex]
+
+    return selectedPrefixText
+  }
+
+  console.log(JSON.stringify(content, null, 4))
+}
 
 start()
